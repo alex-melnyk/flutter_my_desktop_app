bba2dd88cc56a0025191b0141c0b698be2178712
Alex Melnyk <ask.melnyk@gmail.com>
2021-04-09 22:34:48 +0300

    Project files added.

---
+++ android/app/src/main/kotlin/com/example/my_desktop_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_desktop_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Runner/MainFlutterWindow.swift
@@ -0,0 +1,29 @@
+import Cocoa
+import FlutterMacOS
+
+class MainFlutterWindow: NSWindow {
+  override func awakeFromNib() {
+    let flutterViewController = FlutterViewController.init()
+    let windowFrame = self.frame
+    self.contentViewController = flutterViewController
+    self.setFrame(windowFrame, display: true)
+    RegisterGeneratedPlugins(registry: flutterViewController)
+
+    // Adding a NSVisualEffectView to act as a translucent background
+    let contentView = contentViewController!.view;
+    let superView = contentView.superview!;
+
+    let blurView = NSVisualEffectView(frame: superView.bounds)
+    blurView.autoresizingMask = [.width, .height]
+    blurView.blendingMode = NSVisualEffectView.BlendingMode.behindWindow
+
+    // Pick the correct material for the task
+    blurView.material = NSVisualEffectView.Material.underWindowBackground
+
+    // Replace the contentView and the background view
+    superView.replaceSubview(contentView, with: blurView)
+    blurView.addSubview(contentView)
+
+    super.awakeFromNib()
+  }
+}
